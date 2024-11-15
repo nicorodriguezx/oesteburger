93d74b0fb6d7e40d8f3f185f10d546f7f7d93c79
nicorodriguezx <nicorodriguezx@gmail.com>
2024-11-14 22:28:29 -0300

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addGlobalData("googleAnalyticsId", process.env.GOOGLE_ANALYTICS_ID);
+    eleventyConfig.addGlobalData("whatsappLink", process.env.WHATSAPP_LINK);
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/styles");
     return {
